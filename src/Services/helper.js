41daa11e4a4a48e56165f23aec3db132385b9bbc
himanshusingh372 <himanshu372singh@gmail.com>
2023-06-04 01:35:08 +0530

    change it

--- src/Services/helper.js
+++ src/Services/helper.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { getToken } from "../auth";
-export const BASE_URL = "http://blogapplicationbyhs-env.eba-mth3vthu.eu-north-1.elasticbeanstalk.com/api/v1";
+
+export const BASE_URL =
+  "http://blogapplicationbyhs-env.eba-mth3vthu.eu-north-1.elasticbeanstalk.com/api/v1";
 
 export const myAxios = axios.create({
   baseURL: BASE_URL,
@@ -10,10 +12,6 @@ export const privateAxios = axios.create({
   baseURL: BASE_URL,
 });
 
-// const config = {
-//   headers: {}
-// };
-
 privateAxios.interceptors.request.use(
   (config) => {
     const token = getToken();
@@ -21,24 +19,9 @@ privateAxios.interceptors.request.use(
     if (token) {
       config.headers = config.headers || {};
       config.headers.Authorization = `Bearer ${token}`;
-      // console.log(config);
     }
 
     return config;
   },
   (error) => Promise.reject(error)
 );
-
-// privateAxios.interceptors.request.use(
-//   (config) => {
-//     const token = getToken();
-  
-//     if (token) {
-//       config.headers.common.Authorization = `Bearer ${token}`;
-//       // console.log(config);
-//     }
-
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
